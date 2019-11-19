6f2cdf7ffb565835aafb29160ce57adff76d2ef8
Ugochi <amakomitchell5@gmail.com>
2019-11-19 14:26:08 +0100

    passing state as props

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import { returnStatement } from '@babel/types';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
 
-    // This is the ONLY time we do direct assignment
-    // to this.state
-    this.state = { lat: null, errorMessage: '' };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // we called setState!!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
@@ -28,7 +19,7 @@ class App extends React.Component {
       return <div>Error Message: {this.state.errorMessage}</div>;
     }
     if(!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat}/>
     }
     return <div>Loading!</div>;
   }
